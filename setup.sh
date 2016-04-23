f6e3d57893180e184e4a677a0472abb36c556700
NStal <nstalmail@gmail.com>
2016-04-23 14:33:31 +0800

    first blood

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+# add ignores
+git config --global alias.ignore '!echo $2 >> .gitignore'
+git config --global alias.ls '!echo $GIT_PREFIX'
