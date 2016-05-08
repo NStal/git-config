905b84a1d004c4d0b96ec646e3b7940b2cdb5cfe
NStal <nstalmail@gmail.com>
2016-05-08 15:46:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.ci commit
+git config --global alias.ca commit -a
 git config --global alias.br branch
+git config --global alias.fa fetch --all
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
 # add ignores
